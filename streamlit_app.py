b5c4e9ca32b378fc590302301cba87222e084672
zirconat <168637627+zirconat@users.noreply.github.com>
2024-07-21 03:44:02 +0000

    changed main prog

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import random
+# import random
 
 st.set_page_config(
     page_title = "Trips & Visits",
@@ -13,3 +13,23 @@ st.title("✈️ Trips & Visits Outlook")
 st.write(
     "An overview of the trips and visits for WY2024/25."
 )
+
+# Upload file function
+def load_data(file):
+    data = pd.read_excel(file)
+    return data
+
+# Store uploaded file
+uploaded_file = st.file_uploader("Choose a file to begin")
+
+# Check if file uploaded, prevent error message
+if uploaded_file is None:
+    st.info("Please upload a file", icon = "⚠️")
+    st.stop() # stop processes
+
+# Assign uploaded file to dataframe
+df = load_data(uploaded_file)
+
+# Preview data in expanded window
+with st.expander("Data preview"):
+    st.dataframe(df)
