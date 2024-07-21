223c8ead1c430c2233ab8d042b4fd503a24893da
zirconat <168637627+zirconat@users.noreply.github.com>
2024-07-21 03:38:12 +0000

    remove random generator

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import random
-from datetime import datetime
-import openpyxl
 
 st.set_page_config(
     page_title = "Trips & Visits",
