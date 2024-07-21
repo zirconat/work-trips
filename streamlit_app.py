74d589e5174e1b9264f8e21d93e390fc970b766a
zirconat <168637627+zirconat@users.noreply.github.com>
2024-07-21 03:55:07 +0000

    added filters

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ st.write(
     "An overview of the trips and visits for WY2024/25."
 )
 
+@st.cache_data # cache data
 # Upload file function
-@st.cache_data
 def load_data(file):
     data = pd.read_excel(file)
     return data
@@ -30,6 +30,37 @@ if uploaded_file is None:
 # Assign uploaded file to dataframe
 df = load_data(uploaded_file)
 
+
+# Sidebar filter options
+with st.sidebar:
+    st.header("Filter:")
+    country = st.sidebar.multiselect(
+        "Select the country:",
+        options=df["Country"].unique(),
+        default=df["Country"].unique()
+    )
+    service = st.sidebar.multiselect(
+        "Select the service:",
+        options=df["Service"].unique(),
+        default=df["Service"].unique()
+    )
+    status = st.sidebar.multiselect(
+        "Select the status:",
+        options=df["Status"].unique(),
+        default=df["Status"].unique()
+    )
+    level = st.sidebar.multiselect(
+        "Select the level:",
+        options=df["Level"].unique(),
+        default=df["Level"].unique()
+    )
+
+df_selection = df.query(
+    "Country == @country & Service == @service & Status == @status & Level == @level"
+)
+
+
 # Preview data in expanded window
 with st.expander("Data preview"):
-    st.dataframe(df)
+    #st.dataframe(df)
+    st.dataframe(df_selection) # use when filter is on
